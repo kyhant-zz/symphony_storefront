d2416cf59afccabdfb54b6c270a267af416a32e0
Kyhan Turner <kyhanpt@gmail.com>
2016-12-16 10:22:01 -0800

    sorting complete

--- src/components/app.js
+++ src/components/app.js
@@ -18,7 +18,7 @@ export default class App extends Component {
   getData() {
     let self = this;
     getProducts()
-    .then(function(products){
+    .then(products => {
       self.setState({
         pageTitle: products.pageTitle,
         products: products.products,
@@ -26,17 +26,39 @@ export default class App extends Component {
       })
     })
   }
+
+  _filter(e){
+    e.preventDefault();
+    console.log('E',e.target.value)
+    let filterParameter = e.target.value;
+    let productsArr = this.state.products;
+    console.log('filterparma', typeof(filterParameter))
+    productsArr.sort((a,b) => {
+      console.log('AFILTERPARAM', a[filterParameter]);
+      return (a[filterParameter] > b[filterParameter]) ? 1 : ((b[filterParameter] > a[filterParameter]) ? -1: 0);
+    });
+    console.log('PRODUCTSARR', productsArr);
+    this.setState({products: productsArr});
+
+  }
   render() {
     return (
       <div className="main-content">
         <h1>{this.state.pageTitle}</h1>
         <p>{this.state.extraInfo}</p>
+        <select onChange={this._filter.bind(this)}>
+          <option value="name">Name</option>
+          <option value="maxPrice">Price</option>
+          <option value="createdAt">Date</option>
+        </select>
         { this.state.products.map(item => {
+          let date = (new Date(item.createdAt)).toString();
           return <Product
           image={item.mainImage.ref}
           id={item.id}
           name={item.name}
-          price={item.maxPrice} />
+          price={item.maxPrice}
+          date={date} />
         })}
       </div>
     );
